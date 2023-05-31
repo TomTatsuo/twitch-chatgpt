5fb425e5f9dd654ec0008c018a5fb0aa075491a5
TomTatsuo <130252826+TomTatsuo@users.noreply.github.com>
2023-05-31 08:37:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -87,13 +87,23 @@ app.get('/gpt/:text', async (req, res) => {
         console.log ("Agent answer: " + agent_response)
         messages.push({role: "assistant", content: agent_response})
 
-        //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 399)
-          console.log ("Sliced agent answer: " + agent_response)
-        }
+     // Check for Twitch max. chat message length limit and split if needed
+if (agent_response.length > 399) {
+  console.log("Agent answer exceeds Twitch chat limit. Splitting into multiple messages.")
 
+  // Split the agent_response into chunks of 399 characters
+  const chunks = agent_response.match(/.{1,399}/g)
+
+  // Loop through each chunk and send them as separate messages
+  for (let i = 0; i < chunks.length; i++) {
+    console.log("Agent answer part " + (i + 1) + ": " + chunks[i])
+
+    // Send each chunk as a separate message
+    res.send(chunks[i])
+  }
+} else {
+  console.log("Agent answer within Twitch chat limit.")
+  console.log("Agent answer: " + agent_response)
         res.send(agent_response)
       } else {
         res.send("Something went wrong. Try again later!")
